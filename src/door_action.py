3880c06023c7ee11e54a459ae0bb07c360b2b5a8
KIT <KIT.RoboCup.HomePJ@gmail.com>
2019-12-11 20:40:26 +0900

    12.11

--- src/door_action.py
+++ src/door_action.py
@@ -24,14 +24,14 @@ class OpenDoor():
         self.front_value = []
         self._action_server = SimpleActionServer('door_action',
                 OpenDoorAction, 
-                execute_cb = self.execute_cb,
+                execute_cb = self.execute,
                 auto_start=False)
         self.flg= False
         self._action_server.start()
+        self.result = OpenDoorResult()
 
     def laserscanCB(self, receive_msg):
-        self.laser = receive_msg
-        self.front_value = self.laser.ranges[359]
+        self.front_value = receive_msg.ranges[359]
         self.flg = True
 
     def linerContorol(self, value):
@@ -40,23 +40,30 @@ class OpenDoor():
         rospy.sleep(0.1)
         self.pub.publish(twist_cmd)
 
-    def execute_cb(self, goal):
-        rospy.loginfo('start"door_action"')
-        while not rospy.is_shutdown() and self.flg == False:
-            rospy.loginfo('wait for laserscan ...')
-            rospy.sleep(2.0)
-        self.flg = False
-        if self.front_value > 2.0:
-            result = OpenDoorResult()
-            self._action_server.set_succeeded(result=True)
+    def execute(self, goal):
+        try:
+            rospy.loginfo('start"door_action"')
+            while not rospy.is_shutdown() and self.flg == False:
+                rospy.loginfo('wait for laserscan ...')
+                rospy.sleep(2.0)
+            self.flg = False
+            if self.front_value > 2.0:
+                self.result = True
         #    for i in range(10):
-            self.linerContorol(0.1)
-        else:
-            result = OpenDoorResult()
-            self._action_server.set_succeeded(result=False)
+                self.linerContorol(0.1)
+            else:
+                self.result = False
+            self._action_server.set_succeeded(self.result)
+        except rospy.ROSInterruptException:
+            rospy.loginfo('Interrupted')
+            pass
 
             
 if __name__ == '__main__':
-    rospy.init_node('door_action')
-    OpenDoor()
-    rospy.spin()
+    try:
+        rospy.init_node('door_action')
+        OpenDoor()
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        rospy.loginfo('Interrupted')
+        pass
